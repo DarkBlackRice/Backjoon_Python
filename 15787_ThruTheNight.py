f0dd3aef732d2e02e252ffb50515cb1ad9e7b208
전지환 <jeonji0109@naver.com>
2023-07-23 17:02:58 +0900

    230723 15787 solved

--- 15787_ThruTheNight.py
+++ 15787_ThruTheNight.py
@@ -3,8 +3,31 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int,input().split())
-train = [[0]*20 for _ in range(N)]
+trains = [[0]*20 for _ in range(N)]
 cmds = [list(map(int,input().split())) for _ in range(M)]
+cnt = 0
 
 for cmd in cmds:
-    
+    if cmd[0] == 1:
+        trains[cmd[1]-1][cmd[2]-1] = 1
+    elif cmd[0] == 2:
+        trains[cmd[1]-1][cmd[2]-1] = 0
+    elif cmd[0] == 3:
+        trains[cmd[1]-1].pop()
+        trains[cmd[1]-1].insert(0,0)
+    elif cmd[0] == 4:
+        del trains[cmd[1]-1][0]
+        trains[cmd[1]-1].append(0)
+
+# for i in range(N):
+#     print(*trains[i])
+
+psd_train = []
+for train in trains:
+    if train not in psd_train :
+        cnt += 1
+        psd_train.append(train)
+
+print(cnt)
+
+# 삐트연싼으로만뜰어뽀끼씨빨쒸프트키까안빠쪄요
