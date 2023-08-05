1b9eb02f0742f71641595e4ab1934087b5442824
전지환 <jeonji0109@naver.com>
2023-08-05 17:03:56 +0900

    230805 15649 solved and backtracking practice needed

--- 15649_NandM1.py
+++ 15649_NandM1.py
@@ -1,1 +1,20 @@
 # N과 M (1)
+
+def dfs(cnt, ser):
+    if cnt == M:
+        ans.append(ser)
+        return
+    
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            dfs(cnt+1, ser + [lst[i]])
+            visited[i] = False
+    
+N, M = map(int,input().split())
+visited = [False] * N
+lst = list(range(1,N+1))
+ans = []
+dfs(0, [])
+for a in ans:
+    print(*a)
