d9f69cd31454377661de702a5ecb6dc63beed675
전지환 <jeonji0109@naver.com>
2023-08-14 02:39:19 +0900

    230813 BOJ 16234 solved

--- Gold/IV_16234_migration.py
+++ Gold/IV_16234_migration.py
@@ -1,21 +1,35 @@
 # 인구 이동
 
 def ally(row, col):
-    union = [LAND[row][col],(row,col)]
-    stack = [(row,col)]
-    while stack:
-        r, c = stack.pop()
-        visited[r][c] = True
+    union = []
+    stack = []
+    visited[row][col] = True
+    while True:
+
+        dead_end = True    
         for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if (0<=nr<N and 0<=nc<N) and not visited[nr][nc]\
-                (L<=abs(LAND[r][c] - LAND[nr][nc])<=R):
-                stack.append((nr, nc))
+            nr = row + dr[i]
+            nc = col + dc[i]
+            if (0<=nr<N and 0<=nc<N) and (not visited[nr][nc]) and\
+                (L<=abs(LAND[row][col] - LAND[nr][nc])<=R):
+                dead_end = False
+                stack.append((row, col))
+                if not union:
+                    union.append(LAND[row][col])
+                    union.append((row,col))
+                union[0] += LAND[nr][nc]
+                union.append((nr, nc))
+                row, col = nr, nc
+                visited[nr][nc] = True
+                break
 
-                
+        if dead_end:
+            if stack:
+                row, col = stack.pop()
+            else:
+                break         
+
     return union
-
 
 
 N, L, R = map(int,input().split())
@@ -26,34 +40,40 @@ count = 0
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
-print('지도 : ')
-for country in LAND:
-    print(*country)
-print()
+# print('지도 : ')
+# for country in LAND:
+#     print(*country)
+# print()
 
 while flag:
 
     visited = [[False] * N for _ in range(N)]
-    unions = ally()
+    unions = []
+    for r in range(N):
+        for c in range(N):
+            if not visited[r][c]:
+                temp = ally(r, c)
+                if temp:
+                    unions.append(temp)
     
-    print('연합 정보 : ')
-    for u in unions:
-        print(*u)
+    # print('연합 정보 : ')
+    # for u in unions:
+    #     print(*u)
     
     if unions:
-        for u in unions:
-            union_num = len(u) - 1
-            temp = u[0] // union_num
-            for i in range(1, union_num + 1):
-                r, c = u[i]
-                LAND[r][c] = temp
         count += 1
+        for u in unions:
+            union_num = len(u)
+            avg = u[0] // (union_num - 1)
+            for i in range(1, union_num):
+                tr, tc = u[i]
+                LAND[tr][tc] = avg
     else:
         flag = False
-
-    print('지도 : ')
-    for country in LAND:
-        print(*country)
+    
+    # print('지도 : ')
+    # for country in LAND:
+    #     print(*country)
 
 print(count)
 
