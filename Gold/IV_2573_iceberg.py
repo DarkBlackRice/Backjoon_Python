3038cac4c958954f5c27c9e194ac476969471871
전지환 <jeonji0109@naver.com>
2023-08-17 14:39:27 +0900

    230817 2573 in progress

--- Gold/IV_2573_iceberg.py
+++ Gold/IV_2573_iceberg.py
@@ -1,2 +1,25 @@
 # 빙산
+from collections import deque
 
+dr = [0,1,0,-1]
+dc = [1,0,-1,0]
+
+def bfs(row, col):
+    pass
+
+
+N, M = map(int,input().split())
+iceberg = []
+visited = []
+for r in range(N):
+    temp = list(map(int,input().split()))
+    visited.append([0]*M)
+    for c in range(M):
+        if temp[c]:
+            visited[r][c] += 1
+    
+
+
+for r in range(1, N-1):
+    for c in range(1, M-1):
+        pass
